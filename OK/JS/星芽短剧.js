5bab5d9c65007867c4dbcb7e2906d87c05239b07
tushen6 <3393402@qq.com>
2026-04-06 15:44:35 +0800

    Add files via upload

---
+++ OK/JS/星芽短剧.js
@@ -0,0 +1,170 @@
+var rule = { 变量规则 = {
+    类型: '影视',
+    title: '星芽短剧app',
+    host: 'https://app.whjzjx.cn',
+    url: '/cloud/v2/theaterfyfilter',
+网址： '/cloud/v2/theaterfyfilter'，
+
+    filter_url: '/home_page?theater_class_id=fyclass&type=1&{{fl.type or "class2_ids=0"}}&page_num=fypage&page_size=24',
+filter_url： '/home_page？theater_class_id=fyclass&type=1&{{fl.type 或 “class2_ids=0”}}&page_num=fypage&page_size=24'，
+
+    searchUrl: '/v3/search', searchUrl： '/v3/search'，
+    searchable: 2,
+    quickSearch: 1,
+    filterable: 1,
+    filter: 'H4sIAAAAAAAAA6vmUgACJUMlK4VoMBMEquEssGR2aiVQWqmksiBVSQdVKi8xNxUk93zj7qfzutFlyxJzSlNRTMZuA8I4kFlPW1e8bF6BZhbCTJCS5JzE4mKj+MyUYlsDJQx1tZha8dn3snnv0x1NxNpnQrF9T/uXvFjcSqx9phTb97xvw5Pdi1+saHjWTLSthpRb+6xjxvOl84i10IjycH3Z0PZi0VpiLTSnPFxX7n+xrYvoEKXcwqd9bU/7NxGdUCnPGS+2zni6cj/RUWhGeZAunfe0ZzfRFlKeSF+29z6fMp9YC6ngwSlznq5bQKx9xsbUKGxIzvzGVMgbfS1PdxIdkcaUR+SLCT1P180l1kJLyu1rmfhsC9Glm7ERxRY+2bWJhCg0MaTchzMnPGtEr9JxWmiBxT4UkVguVPFYrloA8HqNcnwIAAA=',
+过滤器： 'H4sIAAAAAAAAA6vmUgACJUMlK4VoMBMEquEssGR2aiVQWqmksiBVSQdVKi8xNxUk93zj7qfzutFlyxJzSlNRTMZuA8I4kFlPW1e8bF6BZhbCTJCS5JzE4mKj+MyUYlsDJQx1tZha8dn3snnv0x1NxNpnQrF9T/uXvFjcSqx9phTb97xvw5Pdi1+saHjWTLSthpRb+6xjxvOl84i10IjycH3Z0PZi10IjycH3Z0PZi。 0VpiLTSnPFxX7n+xrYvoEKXcwqd9bU/7NxGdUCnPGS+2zni6cj/RUWhGeZAunfe0ZzfRFlKeSF+29z6fMp9YC6ngwSlznq5bQKx9xsbUKGxIzvzGVMgbfS1PdxIdkcaUR+SLCT1P180l1kJLyu1rmfhsC9Glm7ERxRY+2bWJhCg0MaTchzMnPGtEr9JxWmiBxT4UkVguVPFYrloA8HqNcnwIAAA='，
+
+    headers: {
+        "X-App-Id": "7",
+        "platform": "1",
+        "manufacturer": "Xiaomi", “manufacturer”： “小米”，
+        "version_name": "3.2.0.1", “version_name”： “3.2.0.1”，
+        "user_agent": "Mozilla/5.0 (Linux; Android 11; M2012K10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36",
+“user_agent”： “Mozilla/5.0 （Linux;安卓11;M2012K10C Build/RP1A.200720.011;wv） AppleWebKit/537.36 （KHTML，如 Gecko） 版本/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36“，
+
+        "dev_token": "BY1wFZd4K0vDqzVMbtHXNmlQ29ovO5peS5MsZ7VX1rQUXDwAqskxkJtiPMZCEj6MaW-40xNnbvn12F68nLEAMkRoD7tpoieO4nkUE-GLOYxqCs0xmxWgXAh0-7NmmGCHi95SlyZlpHeit94JfJPkpo-hl4JFru2wUI-4P0AwDc5Y*",
+“dev_token”： “BY1wFZd4K0vDqzVMbtHXNmlQ29ovO5peS5MsZ7VX1rQUXDwAqskxkJtiPMZCEj6MaW-40xNnbvn12F68nLEAMkRoD7tpoieO4nkUE-GLOYxqCs0xmxWgXAh0-7NmmGCHi95SlyZlpHeit94JfJPkpo-hl4JFru2wUI-4P0AwDc5Y*”，
+
+        "app_version": "3.2.0.1", “app_version”： “3.2.0.1”，
+        "device_platform": "android", “device_platform”： “安卓”，
+        "personalized_recommend_status": "1",
+“personalized_recommend_status”： “1”，
+
+        "device_type": "M2012K10C", “device_type”： “M2012K10C”，
+        "device_brand": "Redmi", “device_brand”：“红米”，
+        "os_version": "11", “os_version”： “11”，
+        "channel": "default", “通道”： “默认”，
+        "raw_channel": "default", “raw_channel”： “默认”，
+        "oaid": "9494817a02a93435", “OAID”： “9494817A02A93435”，
+        "msa_oaid": "9494817a02a93435", “msa_oaid”： “9494817a02a93435”，
+        "uuid": "randomUUID_f87c01c7-3d61-4feb-ade0-3d6d45d24dfd",
+“uuid”： “randomUUID_f87c01c7-3d61-4feb-ade0-3d6d45d24dfd”，
+
+        "device_id": "23b07cf840c8b3eeca8c01fc56f0f0a09",
+“device_id”： “23b07cf840c8b3eeca8c01fc56f0f0a09”，
+
+        "support_h265": "1", “support_h265”： “1”，
+        "Host": "app.whjzjx.cn", “Host”： “app.whjzjx.cn”，
+        "Connection": "Keep-Alive", “连接”：“保持活力”，
+        "User-Agent": "okhttp/4.10.0" “用户代理”： “okhttp/4.10.0”
+    },
+    timeout: 5000,
+    class_name: '九月推荐&热播剧&会员专享&星选好剧&新剧&阳光剧场',
+    class_url: '1&2&8&7&3&5', class_url： '1&2&8&7&3&5'，
+    预处理:$js.toString(()=>{
+        let html = post('https://u.shytkjgs.com/user/v1/account/login', {
+让 html = post（'https：//u.shytkjgs.com/user/v1/account/login'， {
+
+        headers: {
+            'User-Agent': 'okhttp/4.10.0', '用户代理'： 'okhttp/4.10.0'，
+            'Accept-Encoding': 'gzip', '接受编码'： 'gzip'，
+            'Content-Type': 'application/x-www-form-urlencoded',
+'内容类型'： 'application/x-www-form-urlencoded'，
+
+            'x-app-id': '7',
+            'platform': '1',
+            'manufacturer': 'realme', '制造商'： 'realme'，
+            'version_name': '3.3.1', 'version_name'： '3.3.1'，
+            'user_agent': 'Mozilla/5.0 (Linux; Android 9; RMX1931 Build/PQ3A.190605.05081124; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Mobile Safari/537.36',
+'user_agent'： 'Mozilla/5.0 （Linux;人造人 9;RMX1931 构建/PQ3A.190605.05081124;wv） AppleWebKit/537.36 （KHTML，如 Gecko） 版本/4.0 Chrome/91.0.4472.114 Mobile Safari/537.36'，
+
+            'app_version': '3.3.1', 'app_version'： '3.3.1'，
+            'device_platform': 'android', 'device_platform'： '安卓'，
+            'personalized_recommend_status': '1',
+'personalized_recommend_status'： '1'，
+
+            'device_type': 'RMX1931', 'device_type'： 'RMX1931'，
+            'device_brand': 'realme', 'device_brand'： 'realme'，
+            'os_version': '9', 'os_version'： '9'，
+            'channel': 'default', '通道'： '默认'，
+            'raw_channel': 'default', 'raw_channel'： '默认'，
+            'oaid': '',
+            'msa_oaid': '',
+            'uuid': 'randomUUID_914e7a9b-deac-4f80-9247-db56669187df',
+'uuid'： 'randomUUID_914e7a9b-deac-4f80-9247-db56669187df'，
+
+            'device_id': '24250683a3bdb3f118dff25ba4b1cba1a',
+'device_id'： '24250683a3bdb3f118dff25ba4b1cba1a'，
+
+            'ab_id': '',
+            'support_h265': '1' 'support_h265'： '1'
+        },
+        body: "device=24250683a3bdb3f118dff25ba4b1cba1a&install_first_open=false&first_install_time=1723214205125&last_update_time=1723214205125&report_link_url="
+正文：“device=24250683a3bdb3f118dff25ba4b1cba1a&install_first_open=false&first_install_time=1723214205125&last_update_time=1723214205125&report_link_url=”
+
+    });
+    rule.headers['authorization'] = JSON.parse(html).data.token
+rule.headers['authorization'] = JSON.parse（html）.data.token
+
+    }),
+    play_parse: true,
+    lazy: $js.toString(() => { 懒惰：$js.toString（（） => {
+        input = { 输入 = {
+            url: input,
+            parse: 0
+        }
+    }),
+    double: true,
+    一级: $js.toString(()=>{
+        let d = []; 让 d = [];
+        let html = request(input,{headers:rule.headers});
+let html = request（input，{headers：rule.headers}）;
+
+        let data = JSON.parse(html).data.list;
+让数据 = JSON.parse（html）.data.list;
+
+        data.forEach(it => { data.forEach（它 => {
+            let id = 'https://app.whjzjx.cn/v2/theater_parent/detail?theater_parent_id='+it.theater.id;
+let id = 'https：//app.whjzjx.cn/v2/theater_parent/detail？theater_parent_id='+it.theater.id;
+
+            d.push({
+            url:id,
+            title:it.theater.title, title：it.theater.title，
+            img:it.theater.cover_url, 图像：it.theater.cover_url，
+            desc:it.theater.theme, desc：it.theater.theme，
+        })
+        })
+       setResult(d);
+    }),
+    二级: $js.toString(()=>{ 二级： $js.toString（（）=>{
+        let urls = []; 让网址 = [];
+        let html = request(input,{headers:rule.headers});
+let html = request（input，{headers：rule.headers}）;
+
+        let data = JSON.parse(html).data;
+让数据 = JSON.parse（html）.data;
+
+        data.theaters.forEach(it => { data.theaters.forEach（它 => {
+            urls.push(it.num+'$'+it.son_video_url);
+urls.push（it.num+'$'+it.son_video_url）;
+
+        })
+    VOD = { 视频点播 = {
+            vod_name: data.title, vod_name：data.title，
+            vod_pic: data.cover_url, vod_pic：data.cover_url，
+            vod_play_from: '九月专属',
+            vod_play_url: urls.join('#') vod_play_url： urls.join（'#'）
+        };
+    }),
+  搜索: $js.toString(()=>{ 搜索： $js.toString（（）=>{
+      let html = post(input,{headers:rule.headers,body:{"text":KEY}})
+让 html = post（input，{headers：rule.headers，body：{“text”：KEY}}）
+
+      let list = JSON.parse(html).data.theater.search_data;
+let list = JSON.parse（html）.data.theater.search_data;
+
+      list.forEach(it =>{ list.forEach（它 =>{
+        let id = 'https://app.whjzjx.cn/v2/theater_parent/detail?theater_parent_id='+it.id;
+let id = 'https：//app.whjzjx.cn/v2/theater_parent/detail？theater_parent_id='+it.id;
+
+        d.push({
+            url:id,
+            title:it.title,
+            img:it.cover_url,
+            content:it.introduction, 内容：it.introduction，
+        })
+      })
+      setResult(d);
+  }),
+}
